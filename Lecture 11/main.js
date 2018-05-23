91a6917f12f3a36abfdf1482f1880bedbd408eee
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-05-23 19:11:42 +0300

    move collection methods to prototype

--- Lecture 11/main.js
+++ Lecture 11/main.js
@@ -59,47 +59,49 @@ var UsersCollection = function(user){
   this.users = [].concat(user);
 }
 
-UsersCollection.prototype.show = function(){
-    this.users.forEach(function(element){
-      console.log(element);
-    });
-    return this
-}
-UsersCollection.prototype.size = function(){
-    return this.users.length;
-}
-UsersCollection.prototype.add = function(user){
-    this.users.push(user);
-    return this
-}
-UsersCollection.prototype.remove = function(user){
-    for(var i = 0; i < this.users.length; i++){
-      if(this.users[i] === user){
-        this.users.splice(i, 1);
-      }
-    }
-    return this
-}
-UsersCollection.prototype.addAll = function(usersArray){
-    for(var i = 0; i < usersArray.length; i++){
-      this.users.push(usersArray[i]);
-    }
-    return this
-}
-UsersCollection.prototype.clear = function(){
-    this.users = [];
-    return this
-}
-UsersCollection.prototype.findBy = function(propertyName, propertyValue){
-    return this.users.filter(function(element, index) {
-      for(var key in element.attributes){
-        if(key===propertyName && element.attributes[key]===propertyValue){
-          return element
+UsersCollection.prototype = {
+  constructor : UsersCollection,
+  show : function(){
+      this.users.forEach(function(element){
+        console.log(element);
+      });
+      return this
+  },
+  size : function(){
+      return this.users.length;
+  },
+  add : function(user){
+      this.users.push(user);
+      return this
+  },
+  remove : function(user){
+      for(var i = 0; i < this.users.length; i++){
+        if(this.users[i] === user){
+          this.users.splice(i, 1);
         }
       }
-  });
-}
-UsersCollection.prototype.sortBy = function(propertyName, order){
+      return this
+  },
+  addAll : function(usersArray){
+      for(var i = 0; i < usersArray.length; i++){
+        this.users.push(usersArray[i]);
+      }
+      return this
+  },
+  clear : function(){
+      this.users = [];
+      return this
+  },
+  findBy : function(propertyName, propertyValue){
+      return this.users.filter(function(element, index) {
+        for(var key in element.attributes){
+          if(key===propertyName && element.attributes[key]===propertyValue){
+            return element
+          }
+        }
+      })
+  },
+  sortBy : function(propertyName, order){
     var sortProp = function(a, b){
         if(a.attributes[propertyName] < b.attributes[propertyName]){
           return -1
@@ -118,6 +120,7 @@ UsersCollection.prototype.sortBy = function(propertyName, order){
        this.users.reverse(); 
     }
     return this.users
+  }
 }
 
 var user1 = new User({
