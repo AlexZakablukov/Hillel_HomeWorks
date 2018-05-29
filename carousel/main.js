c837a950e52dc492ffd75f3178a9b13f1d62ff94
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-05-29 15:55:45 +0300

    complete carousel

--- carousel/main.js
+++ carousel/main.js
@@ -3,97 +3,85 @@
     this.rootElement = rootElement;
     this.carouselContainer = this.rootElement.querySelector(".carousel-container");
     this.list = this.rootElement.querySelector(".carousel-list");
-    this.items = this.list.querySelectorAll("li");
+    this.items = this.list.getElementsByTagName("li");
     this.leftArrow = this.rootElement.querySelector(".left-arrow");
     this.rightArrow = this.rootElement.querySelector(".right-arrow");
     this.amountOfPictures = this.items.length;
-    this.picturesInFrame = 4;
+    this.picturesInFrame = 3;
     this.countOfSlides = 1;
-    this.onePictureWidth = 175;
+    this.onePictureWidth =  this.items[0].querySelector("img").width + 10;
     this.currentMarginLeft = 0;
     this.maxMargin = -this.onePictureWidth*(this.amountOfPictures-this.picturesInFrame);
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
+    this.changeFrame = this.changeFrame.bind(this);
+    this.changeSlideCount = this.changeSlideCount.bind(this);
+    this.frameRange = this.rootElement.getElementsByClassName("frameSize")[0];
+    this.slideRange = this.rootElement.getElementsByClassName("slideLength")[0];
   }
   Carousel.prototype.next = function(event){
-    if(this.currentMarginLeft - this.onePictureWidth * this.countOfSlides > this.maxMargin){
-      this.list.style.marginLeft = this.currentMarginLeft - this.onePictureWidth * this.countOfSlides + "px";
-      this.currentMarginLeft = parseInt(this.list.style.marginLeft);
+    if(this.currentMarginLeft < this.maxMargin/2){
+      
+      this.append();
     }
-    else{
-      this.list.style.marginLeft = this.maxMargin + "px";
-      this.currentMarginLeft = this.maxMargin;
-    }
+    this.list.style.marginLeft = this.currentMarginLeft - this.onePictureWidth * this.countOfSlides + "px";
+    this.currentMarginLeft = parseInt(this.list.style.marginLeft);
   };
   Carousel.prototype.prev = function(event){
-    if(this.currentMarginLeft + this.onePictureWidth * this.countOfSlides < 0){
-      this.list.style.marginLeft = this.currentMarginLeft + this.onePictureWidth * this.countOfSlides + "px";
-      this.currentMarginLeft = parseInt(this.list.style.marginLeft);
+    if(this.currentMarginLeft + this.onePictureWidth * this.countOfSlides > 0){
+      this.prepend();
+      
     }
-    else{
-      this.list.style.marginLeft = 0 + "px";
-      this.currentMarginLeft = 0;
+    this.list.style.marginLeft = this.currentMarginLeft + this.onePictureWidth * this.countOfSlides + "px";
+    this.currentMarginLeft = parseInt(this.list.style.marginLeft);
+  };
+
+  Carousel.prototype.append = function(){
+    for(var i=0; i< this.countOfSlides; i++){
+      this.list.appendChild(this.items[0]);
+      this.currentMarginLeft += this.onePictureWidth;
     }
   };
+
+  Carousel.prototype.prepend = function(){
+    for(var i=0; i< this.countOfSlides; i++){
+    this.list.insertBefore(this.items[this.items.length-1], this.list.firstElementChild);
+    this.currentMarginLeft -= this.onePictureWidth;
+    }
+  };
+  Carousel.prototype.changeFrame = function(){
+    this.picturesInFrame = this.frameRange.valueAsNumber;
+    this.carouselContainer.style.width = this.onePictureWidth * this.picturesInFrame + 15 + "px";
+    this.maxLength = -this.onePictureWidth*(this.amountOfPictures-this.picturesInFrame);
+  }
+  Carousel.prototype.changeSlideCount = function(){
+    this.countOfSlides = this.slideRange.valueAsNumber;
+  }
 
   Carousel.prototype.render = function(){
       this.carouselContainer.style.width = this.onePictureWidth * this.picturesInFrame + 15 + "px";
       this.rightArrow.addEventListener("click", this.next);
       this.leftArrow.addEventListener("click", this.prev)
+      this.frameRange.addEventListener("change", this.changeFrame);
+      this.slideRange.addEventListener("change", this.changeSlideCount);
   }
 
 
- var carousel = new Carousel(document.getElementById("carousel"));
+ var carousel = new Carousel(document.getElementById("carousel-wrap1"));
   carousel.render();
-  var carousel2 = new Carousel(document.getElementById("carousel2"));
+  var carousel2 = new Carousel(document.getElementById("carousel-wrap2"));
   carousel2.render();
 }
-// //настраиваемое пользователем
-//   var picturesInFrame = 3;
-//   var countOfSlides = 1;
-//   var onePictureWidth = 175;
-//   //вычисления
-//   var carousel = document.querySelector(".carousel");
-//   var list = carousel.querySelector(".carousel-list");
-//   var items = list.querySelectorAll("li");
-//   var leftArrow = carousel.querySelector(".left-arrow");
-//   var rightArrow = carousel.querySelector(".right-arrow");
-//   var amountOfPictures = items.length;
-//   var carouselContainer = carousel.querySelector(".carousel-container");
-//   carouselContainer.style.width = onePictureWidth * picturesInFrame + 15 + "px";
-//   var maxLength = -onePictureWidth*(amountOfPictures-picturesInFrame);
-//   var currentMarginLeft = 0;
-//   // var frameSize = document.getElementById("frameSize");
-//   // var slideLength = document.getElementById("slideLength");
+
+  // var frameSize = document.getElementById("frameSize");
+  // var slideLength = document.getElementById("slideLength");
 
 
-//   // frameSize.addEventListener("change", function(event){
-//   //   picturesInFrame = frameSize.valueAsNumber;
-//   //   carouselContainer.style.width = onePictureWidth * picturesInFrame + 15 + "px";
-//   //   maxLength = -onePictureWidth*(amountOfPictures-picturesInFrame);
-//   // })
+  // frameSize.addEventListener("change", function(event){
+  //   picturesInFrame = frameSize.valueAsNumber;
+  //   
+  // })
 
-//   // slideLength.addEventListener("change", function(event){
-//   //   countOfSlides = slideLength.valueAsNumber;
-//   // })
-
-//   rightArrow.addEventListener("click", function(event){
-//     if(currentMarginLeft - onePictureWidth*countOfSlides > maxLength){
-//       list.style.marginLeft = currentMarginLeft - onePictureWidth*countOfSlides + "px";
-//       currentMarginLeft = parseInt(list.style.marginLeft);
-//     }
-//     else{
-//       list.style.marginLeft = maxLength + "px";
-//       currentMarginLeft = maxLength;
-//     }
-//   })
-//   leftArrow.addEventListener("click", function(event){
-//     if(currentMarginLeft + onePictureWidth*countOfSlides < 0){
-//       list.style.marginLeft = currentMarginLeft + onePictureWidth*countOfSlides + "px";
-//       currentMarginLeft = parseInt(list.style.marginLeft);
-//     }
-//     else{
-//       list.style.marginLeft = 0 + "px";
-//       currentMarginLeft = 0;
-//     }
-//   })
+  // slideLength.addEventListener("change", function(event){
+  //   countOfSlides = slideLength.valueAsNumber;
+   
