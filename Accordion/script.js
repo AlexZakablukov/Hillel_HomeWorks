d8904a3b55778dcf8407a6602a9a97bfd1e87947
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-06-01 00:58:28 +0300

    add Accordion

---
+++ Accordion/script.js
@@ -0,0 +1,36 @@
+var Accordion = function(rootElement){
+	this.rootElement = rootElement;
+	this.headings = this.rootElement.getElementsByClassName("accordion-heading");
+	this.headingItems = [].slice.call(this.headings);
+	this.contents = this.rootElement.getElementsByClassName("accordion-content");
+	this.contentItems = [].slice.call(this.contents);
+	this.setActive	= this.setActive.bind(this);
+
+};
+Accordion.prototype.classNames = {
+	active : "accordion-heading-active",
+	hide : "accordion-content-hide", 
+};
+
+Accordion.prototype.setActive = function(event){
+	for(var i = 0; i < this.headingItems.length; i++){
+			if(this.headingItems[i] === event.target){
+				this.headingItems[i].classList.toggle(this.classNames.active);
+				this.contentItems[i].classList.toggle(this.classNames.hide);
+			}
+			else{
+				this.headingItems[i].classList.remove(this.classNames.active);
+				this.contentItems[i].classList.add(this.classNames.hide);
+			}
+		}
+};
+
+Accordion.prototype.delegateEvent = function(){
+	this.rootElement.addEventListener("click", this.setActive);
+	return this
+};
+
+Accordion.prototype.render = function(){
+	this.delegateEvent();
+	return this
+};
