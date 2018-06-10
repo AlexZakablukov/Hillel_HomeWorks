e845d038a9157a01d2973892e384d206b2544c60
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-06-10 09:28:24 +0300

    make sliders

---
+++ 777capital/build/assets/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  $('.information__carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    center: false,
+    items: 2,
+
+    responsive:{
+        0:{
+            items:1,
+            dots: true
+        },
+        576:{
+            items:2,
+            dots: true
+        },
+        766:{
+            items:2,
+            dots: true
+        },
+        992:{
+            items:4,
+            dots: false,
+            loop: false,
+            mouseDrag: false,
+        }
+    }
+    });
+  $('.services__carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    center: false,
+    items: 2,
+
+    responsive:{
+        0:{
+            items:1,
+            dots: true
+        },
+
+        576:{
+            items:2,
+            dots: true
+        },
+        766:{
+            items:3,
+            dots: false,
+            loop: false,
+            mouseDrag: false,
+        }
+    
+        }
+    });
+});
