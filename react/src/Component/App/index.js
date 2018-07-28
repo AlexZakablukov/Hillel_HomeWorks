e6b1335cb70a75bde54f7bfcab0845d60cc89935
Sasha Zakablukov <Zakablukov777@gmail.com>
2018-07-28 13:27:51 +0300

    add withIcon hoc

--- react/src/Component/App/index.js
+++ react/src/Component/App/index.js
@@ -3,7 +3,7 @@ import './style.css';
 import Button from "../Button";
 import Link from "../Link";
 import Icon from "../Icon";
-
+import withIcon from  "../hocs/withIcon"
 
 class App extends Component {
   constructor(props){
@@ -24,19 +24,27 @@ class App extends Component {
   
 
   render() {
+      const ButtonWithIcon = withIcon("Button")(Button);
+      const LinkWithIcon = withIcon("Link")(Link);
     return (
       <div className="App">
         <Button onClick={this.toggle}>click me</Button>
-        <br/>
+          <br/>
         <Button block = {this.state.isBlock} color="green">click me</Button>
         <br/>
         <Button block = {this.state.isBlock} color="red">click me</Button>
+          <hr/>
         <br/>
         <Link className="Link--red">adsad</Link>
         <br/>
         <Link className="Link--blue" to="http://www.google.com">google</Link>
         <hr/>
         <Icon name="google"></Icon>
+          <hr/>
+        <ButtonWithIcon iconName={"google"} color={"black"}>Google</ButtonWithIcon>
+        <br/>
+        <LinkWithIcon iconName={"google"} to={"http://www.google.com"} target={"_blank"}>Google</LinkWithIcon>
+
       </div>
     );
   }
---
+++ react/src/Component/hocs/withIcon.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Icon from "../Icon";
+
+
+const withIcon =(moduleName)=> {
+    return (WrappedComponent) => {
+        const ComponentWithIcon = ({children, iconName, ...props}) => {
+            return <WrappedComponent {...props}>
+                <div className={`${moduleName}-icon`}>
+                    <Icon name = {iconName}/>
+                </div>
+                <div className={`${moduleName}-content`}>
+                    {children}
+                </div>
+            </WrappedComponent>
+        };
+        ComponentWithIcon.propTypes ={
+            children: PropTypes.node,
+            iconName: PropTypes.string.isRequired,
+        };
+        ComponentWithIcon.defaultProps ={
+            children: "",
+        };
+
+        return ComponentWithIcon;
+    };
+};
+
+export default withIcon;
